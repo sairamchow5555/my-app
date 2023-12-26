dc67c48ff02ff0bf44185adfeef4923e804fcc86
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-12-26 09:33:01 +0530

    jwt profile

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { jwtConstants } from "./auth.constants";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+
+    constructor() {
+        super({
+          jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+          ignoreExpiration: false,
+          secretOrKey: jwtConstants.secret,
+        });
+      }
+
+    async validate(payload: any){
+        return {id: payload.sub, email: payload.email}
+    }
+}
